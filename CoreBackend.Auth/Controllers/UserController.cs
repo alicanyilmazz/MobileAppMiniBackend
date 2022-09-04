5d96336ad98fb5490c8464d3d22d68ccd33b0a36
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-04 17:32:59 +0300

    Controller created.

---
+++ CoreBackend.Auth/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBackend.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
