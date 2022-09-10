8f372c26aefe798592754547d164115c64c5be4a
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-10 21:15:20 +0300

    SignUpViewModel added.

---
+++ CoreBackend.Auth/Models/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreBackend.Auth.Models
+{
+    public class SignUpViewModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+    }
+}
