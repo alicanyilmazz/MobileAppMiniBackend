0ed139cfb34e0977b08b1e010d4dc899a978834e
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-10 22:01:08 +0300

    SignUp,GetUser added.

--- CoreBackend.Auth/Controllers/UserController.cs
+++ CoreBackend.Auth/Controllers/UserController.cs
@@ -1,23 +1,65 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CoreBackend.Auth.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
 
 namespace CoreBackend.Auth.Controllers
 {
+    [Authorize(LocalApi.PolicyName)]
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
-        [Authorize(LocalApi.PolicyName)]
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+      
         public IActionResult Test()
         {
             return Ok("test ok");
         }
 
-        public IActionResult SignUp()
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel signUpViewModel)
         {
-            return Ok("test ok");
+            var user = new ApplicationUser(); // Veritabanındaki User tablomuza karsılık gelen entity miz budur.
+            user.UserName = signUpViewModel.UserName;
+            user.Email = signUpViewModel.Email;
+            user.Country = signUpViewModel.Country;
+            var result = await _userManager.CreateAsync(user,signUpViewModel.Password);
+
+            if (!result.Succeeded)
+            {
+                // TODO : ERRORS
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null)
+                return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null)
+                return BadRequest();
+
+            var userDto = new ApplicationUser();
+
         }
     }
 }
