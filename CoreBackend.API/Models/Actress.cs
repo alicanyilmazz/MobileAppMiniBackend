f3055bfeef629d8afd98a52a123c1a459bae4919
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-11 20:03:29 +0300

    Actress created.

---
+++ CoreBackend.API/Models/Actress.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreBackend.API.Models
+{
+    public class Actress
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<Movie> Movies { get; set; } = new();
+    }
+}
