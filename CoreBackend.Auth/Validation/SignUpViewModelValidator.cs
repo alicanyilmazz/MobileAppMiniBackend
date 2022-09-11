85407a5b5a354c1f227227fa45b5fe8caea4defd
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-11 13:29:57 +0300

    SignUpViewModelValidator created.

---
+++ CoreBackend.Auth/Validation/SignUpViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using CoreBackend.Auth.Models;
+using FluentValidation;
+
+namespace CoreBackend.Auth.Validation
+{
+    public class SignUpViewModelValidator:AbstractValidator<SignUpViewModel>
+    {
+        public SignUpViewModelValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.").EmailAddress().WithMessage("Email format is not correct.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.");
+            RuleFor(x => x.Country).NotEmpty().WithMessage("Country is required.");
+        }
+    }
+}
