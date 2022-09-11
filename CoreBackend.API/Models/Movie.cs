68a212c8a2aa49c1eacf7e2b40dfe5aba5cd6e72
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-11 20:03:46 +0300

    Movie created.

---
+++ CoreBackend.API/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreBackend.API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string PhotoPath { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Actress> Actresses { get; set; } = new List<Actress>();
+    }
+}
