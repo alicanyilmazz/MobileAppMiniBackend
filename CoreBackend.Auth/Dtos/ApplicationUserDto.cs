5bcaa030e03b3f544d258651da5e4b54bc6cd492
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-11 12:17:47 +0300

    ApplicationUserDto added.

---
+++ CoreBackend.Auth/Dtos/ApplicationUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreBackend.Auth.Dtos
+{
+    public class ApplicationUserDto
+    {
+        public string UserName { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+    }
+}
