bfa3cd59d46d42f3577d7289b50ab17d0106d1e0
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-24 14:43:58 +0300

    BaseProperties created.

---
+++ CoreBackend.API/Models/BaseProperties.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreBackend.API.Models
+{
+    public class BaseProperties
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
