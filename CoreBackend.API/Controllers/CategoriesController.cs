2856518e1ea81c3943fa87183cf52c8dde489680
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-25 21:53:09 +0300

    Categories Controller added.

---
+++ CoreBackend.API/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using CoreBackend.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace CoreBackend.API.Controllers
+{
+    public class CategoriesController : ODataController
+    {
+        private readonly AppDbContext _context;
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // odata/categories
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_context.Categories.AsQueryable());
+        }
+    }
+}
