28e4214eefc05c2d01046156e15fd365fce652d5
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-25 21:55:38 +0300

    SqlServer and OData added.

--- CoreBackend.API/Program.cs
+++ CoreBackend.API/Program.cs
@@ -1,19 +1,37 @@
+using CoreBackend.API.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+builder.Services.AddControllers().AddOData(opt => opt.AddRouteComponents("odata", GetEdmModel()).Select().Expand().OrderBy().Count().Filter()); //Select().Expand().OrderBy().Count().Filter()
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +41,15 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+IEdmModel GetEdmModel()
+{
+    var build = new ODataConventionModelBuilder();
+    build.EntitySet<Movie>("Movies");
+    build.EntitySet<Actress>("Actress");
+    build.EntitySet<Category>("Categories");
+    return build.GetEdmModel();
+}
+
+
+
