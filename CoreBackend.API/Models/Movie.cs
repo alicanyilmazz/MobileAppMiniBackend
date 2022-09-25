d2b1ca07e275baff491b6706b601e14f11047d37
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-09-25 21:54:35 +0300

    Properties modified.

--- CoreBackend.API/Models/Movie.cs
+++ CoreBackend.API/Models/Movie.cs
@@ -4,8 +4,8 @@
     {
         public string Name { get; set; }
         public string Author { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string PhotoPath { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? PhotoPath { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public ICollection<Actress> Actresses { get; set; } = new List<Actress>();
