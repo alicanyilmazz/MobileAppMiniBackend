f0e34efcbd38b532568e5b65dd2a76dc9a434543
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:56:15 +0300

    Models created.

---
+++ CoreBackend.Shared/Models/ErrorDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBackend.Shared.Models
+{
+    public class ErrorDto
+    {
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public List<string> Errors { get; set; }
+        public int Status { get; set; }
+        public bool IsShow { get; set; }
+    }
+}
