10f109896f955aa2e125f6e0ca5869bd6e34de86
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:53:23 +0300

    CategoriesValidator created.

---
+++ CoreBackend.API/Validation/CategoriesValidator.cs
@@ -0,0 +1,13 @@
+﻿using CoreBackend.API.Models;
+using FluentValidation;
+
+namespace CoreBackend.API.Validation
+{
+    public class CategoriesValidator : AbstractValidator<Category>
+    {
+        public CategoriesValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty!");
+        }
+    }
+}
