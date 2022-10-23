3d02162ef6a057f9e3446370c2abba23abf0840f
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:53:37 +0300

    MoviesValidator created.

---
+++ CoreBackend.API/Validation/MoviesValidator.cs
@@ -0,0 +1,14 @@
+﻿using CoreBackend.API.Models;
+using FluentValidation;
+
+namespace CoreBackend.API.Validation
+{
+    public class MoviesValidator:AbstractValidator<Movie>
+    {
+        public MoviesValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty!");
+            RuleFor(x => x.Author).NotEmpty().WithMessage("Author can not be empty!");
+        }
+    }
+}
