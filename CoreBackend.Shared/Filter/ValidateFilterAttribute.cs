82e8ea36d1511e91fc68f92537469d4a4549eb0e
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:56:02 +0300

    ValidateFilterAttribute created.

---
+++ CoreBackend.Shared/Filter/ValidateFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using CoreBackend.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBackend.Shared.Filter
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.Where(x => x.Errors.Count > 0).SelectMany(x => x.Errors).Select(x => x.ErrorMessage);
+                ErrorDto errorDto = new ErrorDto();
+                errorDto.Errors.AddRange(errors);
+                errorDto.Status = 400;
+                errorDto.IsShow = true;
+
+                context.Result = new BadRequestObjectResult(errorDto);
+            }
+        }
+    }
+}
