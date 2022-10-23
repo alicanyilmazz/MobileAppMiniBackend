f8c5debd17fb21851c46424cdb602caffa5a7bf1
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:55:33 +0300

    UseCustomExceptionHandler created.

---
+++ CoreBackend.Shared/Extension/UseCustomExceptionHandler.cs
@@ -0,0 +1,45 @@
+﻿using CoreBackend.Shared.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBackend.Shared.Extension
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        var ex = error.Error;
+                        ErrorDto errorDto = new ErrorDto();
+                        errorDto.Errors.Add(ex.Message);
+                        if (ex is CustomException)
+                        {
+                            errorDto.IsShow = true;
+                        }
+                        else
+                        {
+                            errorDto.IsShow = false;
+                        }
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto));
+                    }
+                });
+            });
+        }
+    }
+}
