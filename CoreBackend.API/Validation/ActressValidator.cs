23c6f07870935af0becb94fa2e79909ba202b725
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-10-23 22:53:09 +0300

    Actress validator created.

---
+++ CoreBackend.API/Validation/ActressValidator.cs
@@ -0,0 +1,13 @@
+﻿using CoreBackend.API.Models;
+using FluentValidation;
+
+namespace CoreBackend.API.Validation
+{
+    public class ActressValidator : AbstractValidator<Actress>
+    {
+        public ActressValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty!");
+        }
+    }
+}
